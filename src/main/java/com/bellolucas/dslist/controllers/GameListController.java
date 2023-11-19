8cbcf5660da11663fdd4f6a74a51ee95a0bef9cb
belloLucas <lucasbellodev@gmail.com>
2023-11-19 16:09:05 -0300

    feat: Replacement functionality done

--- src/main/java/com/bellolucas/dslist/controllers/GameListController.java
+++ src/main/java/com/bellolucas/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.bellolucas.dslist.controllers;
 
 import com.bellolucas.dslist.dto.GameListDTO;
 import com.bellolucas.dslist.dto.GameMinDTO;
+import com.bellolucas.dslist.dto.ReplacementDTO;
 import com.bellolucas.dslist.services.GameListService;
 import com.bellolucas.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping("/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
---
+++ src/main/java/com/bellolucas/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,11 @@
+package com.bellolucas.dslist.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReplacementDTO {
+   private Integer sourceIndex;
+   private Integer destinationIndex;
+}
